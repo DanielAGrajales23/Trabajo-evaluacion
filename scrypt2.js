40c7d675a8e0ce7ee2bba5262a5aff752e77f94d
DanielAGrajales23 <dgrajales690mail.com>
2023-04-20 15:40:41 -0500

    Trabajo hoy

---
+++ scrypt2.js
@@ -0,0 +1,7 @@
+let not1,not2,not3,promedio
+not1=Number(prompt("Ingrse nota1:"))
+not2=Number(prompt("Ingrese nota2"))
+not3=Number(prompt("Ingrese nota3"))
+//Proceso
+promedio=(not1+not2+not3)/3
+alert("Promedio total es:"+promedio)
